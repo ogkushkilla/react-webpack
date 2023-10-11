0555fc45c3d2e24baee32c59c8cb5b0873b1e4be
ogkushkilla <vsevolod.kravchenko@yandex.ru>
2023-10-11 19:30:17 +0500

    init react app with webpack

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import image from './img/image.png';
+
+const Module = () => (
+  <>
+    <h1>Hello, React</h1>
+    <img width={200} src={image} />
+  </>
+)
+
+export default Module;
